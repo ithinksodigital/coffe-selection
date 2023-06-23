2f7bee0f8017953bba844db63cd11146a06378d6
Rafal Pawelec (PL) <rafal.pawelec@pwc.com>
2023-06-23 12:42:43 +0200

    Initial Commit

---
+++ CoffeeSearch/CoffeeSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeSearchApp.swift
+//  CoffeeSearch
+//
+//  Created by Rafal Pawelec on 23/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
